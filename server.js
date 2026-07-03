d15589b8458c995a7904325cf67d6ce7376e98fa
sahasra123957 <24wh1a05j4@bvrithyderabad.edu.in>
2026-07-03 13:41:18 +0530

    changed port

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express =require('express')
 const app=express()
-const PORT=3002;
+const PORT=process.env.PORT || 3002
 app.use(express.static("frontend"))
 app.listen(PORT,function(){
     console.log("succesfully running at http://localhost:"+PORT)
