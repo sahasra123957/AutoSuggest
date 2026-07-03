cf9fb30f7aea22bf1fdfc155779d57f1b7e7dd12
sahasra123957 <24wh1a05j4@bvrithyderabad.edu.in>
2026-07-03 12:09:49 +0530

    added html file

---
+++ server.js
@@ -0,0 +1,7 @@
+const express =require('express')
+const app=express()
+const PORT=3002;
+app.use(express.static("frontend"))
+app.listen(PORT,function(){
+    console.log("succesfully running at http://localhost:"+PORT)
+})
