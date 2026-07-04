59d49ce8e99239c3130b95ce454c49eec90cf855
sahasra123957 <24wh1a05j4@bvrithyderabad.edu.in>
2026-07-04 10:15:51 +0530

    changed card

--- frontend/index.js
+++ frontend/index.js
@@ -1,2 +1,48 @@
 console.log("hello from js");
 //alert("hello from js");
+const users = [
+  {
+    "name" :"Sahasra",
+    "gender" : "Female",
+    "image" : "./images/images.jpg"
+  },
+  {
+    "name" :"Shiva",
+    "gender" : "Male",
+    "image" : "./images/shiva.avif"
+  },
+]
+let curUserId = 0;
+function toggle(){
+  if(curUserId == 0)
+    curUserId=1;
+  else
+    curUserId=0;
+  // image
+  document.getElementById("images").src=users[curUserId].image;
+  document.getElementById("card-name").innerText=users[curUserId].name;
+  document.getElementById("card-gender").innerText=users[curUserId].gender;
+}
+function getRandomUser()
+{
+  fetch("https://randomuser.me/api")
+    .then(function (data){
+      return data.json()
+    })
+    .then(function (parsedData){
+      // console.log(parsedData)
+      let gender = parsedData.results[0].gender;
+
+      let first = parsedData.results[0].name.first;
+      let last = parsedData.results[0].name.last;
+      let name = first  + " " + last;
+      let imageUrl = parsedData.results[0].picture.large;
+
+      // gender
+      document.getElementById("card-gender").innerText=gender;
+      // name
+      document.getElementById("card-name").innerText=name;
+      // img
+      document.getElementById("images").src=imageUrl;
+    })
+}
